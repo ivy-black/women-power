7f6187b555354015af98637122d991c4c95cd62b
Belenkaya <ibelenkaya@scnsoft.com>
2019-11-03 20:28:34 +0300

    js

---
+++ ira_pjs/javascript/calculator/calculator.js
@@ -0,0 +1,9 @@
+function calc() {
+  // var x = this.x;
+  var x = document.getElementsByClassName('num');
+
+  //
+  //console.log(document.getElementById(this).i);
+  //   var x = this.getAttribute('value');
+  console.log(x);
+}
